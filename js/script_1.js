c73260247500fc57cbdc8058bb6c2d3ec04da52a
Arsenii <supratuner@mail.ru>
2024-11-08 18:47:31 +0700

    Version 1.1
    Final edition

--- js/script_1.js
+++ js/script_1.js
@@ -1,17 +1,29 @@
 document.getElementById("send_1").onclick = exercise_1; //Переход по клику
 function exercise_1(){ //Объявление функции
-    let number_1 = document.getElementById("name_a_1").value; //Получение переменной по id
-    let number_2 = document.getElementById("name_b_1").value; //Получение переменной по id
-    let number_3 = document.getElementById("name_c_1").value; //Получение переменной по id
-    if (isNaN(number_1) && isNaN(number_2) && isNaN(number_3)){ //Объявление условия на проверку
-        alert("Введите ещё раз корректное число"); //Вывод о несоответствии ввода
-    } else { //Если все верно, тогда
-        if ((number_1 < number_2) && (number_2 < number_3)){ //Объявление условия на проверку
-            document.getElementById("output_exercise_1").innerHTML = "TRUE"; //Вывод
-            return; //Возвращение функции
-        } else{ //Если не прошел по условию проверки, тогда
-            document.getElementById("output_exercise_1").innerHTML = "FALSE"; //Вывод
-            return; //Возвращение функции
+    let number_1 = document.getElementById("name_n_1").value; //Получение переменной по id
+    let number_2 = document.getElementById("name_set_1").value; //Получение переменной по id
+    let arr_number_2 = number_2.split(", ") //Переопределение переменной в массив
+    let answer = ''; //Объявление переменной для ответа
+    let k = 0; //Объявление счетчика четных чисел
+    let i; //Объявление переменной для цикла
+    if (isNaN(number_1) || (number_1 == "")){ //Объявление условия на проверку
+        alert("Введите ещё раз корректное число N"); //Вывод о несоответствии ввода
+        return;
+    } else { //Если с первичной проверкой все хорошо, тогда
+        for (i = 0; i < number_1; i++) { //Цикл
+            if (isNaN(arr_number_2[i]) || arr_number_2[i] == "" || Number(number_1) != arr_number_2.length) { //Проверка
+                alert("Введите ещё раз корректные числа в Set"); //Вывод
+                return;
+            }     
         }
+        for (i = 0; i < number_1; i++) { //Цикл
+            if (arr_number_2[i] % 2 == 0) { //Проверка четности
+                answer += arr_number_2[i] + ", "; //Формирование ответа
+                k++; //Счетчик количества
+            }
+        }
+        document.getElementById("output_exercise_1_1").innerHTML = "Четные цифры: " + answer.slice(0, -2) + "."; //Вывод
+        document.getElementById("output_exercise_1_2").innerHTML = "Их количество: " + k + "."; //Вывод
+        return; 
     }
 }
--- js/script_2.js
+++ js/script_2.js
@@ -1,15 +1,23 @@
 document.getElementById("send_2").onclick = exercise_2; //Переход по клику
 function exercise_2(){ //Объявление функции
-    var number_1 = document.getElementById("name_a_2").value; //Получение переменной по id
-    var number_2 = document.getElementById("name_b_2").value; //Получение переменной по id
-    if (isNaN(number_1) && isNaN(number_2)){ //Объявление условия на проверку
-        alert("Введите ещё раз корректное число"); // Вывод о несоответствии ввода
+    let number_1 = document.getElementById("name_a_2").value; //Получение переменной по id
+    let number_2 = document.getElementById("name_b_2").value; //Получение переменной по id
+    let amount = Number(number_2); //Объявление переменной для суммы
+    let syllable = Number(number_1); //Объявление переменной для сложения
+    let i; //Объявление переменной для цикла
+    if (isNaN(number_1) && isNaN(number_2) || (number_1 == "" || number_2 == "")){ //Объявление условия на проверку
+        alert("Введите ещё раз корректное число"); //Вывод о несоответствии ввода
+        return; 
     } else { //Если все верно, тогда
-        if (number_1 < number_2){ //Объявление условия на проверку
-            document.getElementById("output_exercise_2").innerHTML = "Первое число меньше"; //Вывод
+        if (Number(number_1) < Number(number_2)){ //Объявление условия на проверку
+            for(i = 0; i < (number_2 - number_1); i++) { //Цикл
+                amount += syllable; //Сложение
+                syllable++; //Прибавление
+            }
+            document.getElementById("output_exercise_2").innerHTML = "Сумма всех целых чисел: " + amount + "."; //Вывод
             return; 
-        } else{ //Если не прошел по условию проверки, тогда
-            document.getElementById("output_exercise_2").innerHTML = "Второе число меньше"; //Вывод
+        } else { //Если не прошел по условию проверки, тогда
+            alert("Введите ещё раз корректные числа"); //Вывод
             return;
         }
     }
